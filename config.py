871defe050791206f12b4d0b007e75f46cdfd01c
VladRoJa <vladimirodriguez2329@gmail.com>
2025-05-05 06:21:04 -0700

    v1.1

--- config.py
+++ config.py
@@ -37,7 +37,8 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
 
     # CORS
     CORS_ORIGINS = [
