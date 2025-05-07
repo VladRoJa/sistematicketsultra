2913741eda8163c63924eb9a745c050dcddcfc8c
VladRoJa <vladimirodriguez2329@gmail.com>
2025-05-07 08:52:17 -0700

    usando hash

--- frontend-angular/src/main.ts
+++ frontend-angular/src/main.ts
@@ -1,6 +1,7 @@
-// main.ts
+//C:\Users\Vladimir\Documents\Sistema tickets\frontend-angular\src\main.ts
+
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
@@ -12,26 +13,23 @@ import { importProvidersFrom } from '@angular/core';
 import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
 import { JwtInterceptor } from './app/interceptors/jwt.interceptor';
 
-
-
 bootstrapApplication(AppComponent, {
   providers: [
     provideAnimations(),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()), // 🔧 Aquí el cambio
     importProvidersFrom(MatDialogModule),
     provideHttpClient(withInterceptorsFromDi()),
     {
-        provide: HTTP_INTERCEPTORS,
-        useClass: TokenInterceptor,
-        multi: true
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
     },
     {
-        provide: HTTP_INTERCEPTORS,
-        useClass: JwtInterceptor,
-        multi: true
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
     },
     { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
-    JwtHelperService,
-    provideAnimations()
-]
+    JwtHelperService
+  ]
 }).catch(err => console.error(err));
