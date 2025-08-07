37a4ff5e9d1a7b0a4ced6f9a92e5c373ee5a0784
VladRoJa <vladimirodriguez2329@gmail.com>
2025-08-07 10:02:09 -0700

    environment

--- frontend-angular/src/environments/environment.ts
+++ frontend-angular/src/environments/environment.ts
@@ -1,6 +1,6 @@
-// environment.ts
+// frontend-angular\src\environments\environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api'
+  apiUrl: 'http://184.107.165.75:5000/api'
 };
 
