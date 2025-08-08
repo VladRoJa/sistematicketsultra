423d43d89b0d4beac49955d4d4500b7195374e9d
VladRoJa <vladimirodriguez2329@gmail.com>
2025-08-07 17:51:57 -0700

    .routes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -9,21 +9,21 @@ import os
 
 load_dotenv()
 from .config import Config
-from extensions import db, migrate
-from routes import auth_bp, ticket_bp, main_bp
-from routes.inventarios import inventario_bp
-from routes.permisos_routes import permisos_bp
-from routes.departamentos_routes import departamentos_bp
-from routes.reportes import reportes_bp
-from routes.sucursales import sucursales_bp
-from routes.importar_inventario import bp_importar
-from routes.asistencia_routes import asistencia_bp
-from routes.horarios_routes import horarios_bp
-from routes.bloques_routes import bloques_bp
-from routes.asignacion_horario_routes import asignacion_bp
-from routes.catalogos_routes import catalogos_bp
-from routes.usuarios_routes import usuarios_bp
-from routes.formulario_ticket_routes import formulario_ticket_bp
+from .extensions import db, migrate
+from .routes import auth_bp, ticket_bp, main_bp
+from .routes.inventarios import inventario_bp
+from .routes.permisos_routes import permisos_bp
+from .routes.departamentos_routes import departamentos_bp
+from .routes.reportes import reportes_bp
+from .routes.sucursales import sucursales_bp
+from .routes.importar_inventario import bp_importar
+from .routes.asistencia_routes import asistencia_bp
+from .routes.horarios_routes import horarios_bp
+from .routes.bloques_routes import bloques_bp
+from .routes.asignacion_horario_routes import asignacion_bp
+from .routes.catalogos_routes import catalogos_bp
+from .routes.usuarios_routes import usuarios_bp
+from .routes.formulario_ticket_routes import formulario_ticket_bp
 
 
 
