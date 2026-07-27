78cc57fcffc17a18590d1271d6711f6c48f28405
VladRoJa <vladimirodriguez2329@gmail.com>
2026-07-26 19:01:31 -0700

    chore: retirar detalle heredado de control de rutinas

--- frontend/src/app/routine-control/routine-control.routes.ts
+++ frontend/src/app/routine-control/routine-control.routes.ts
@@ -7,11 +7,6 @@ export const ROUTINE_CONTROL_ROUTES: Routes = [
       .then((m) => m.RoutineControlDashboardComponent),
   },
   {
-    path: 'socios/:memberId',
-    loadComponent: () => import('./routine-control-member-detail/routine-control-member-detail.component')
-      .then((m) => m.RoutineControlMemberDetailComponent),
-  },
-  {
     path: 'corridas',
     loadComponent: () => import('./routine-control-runs/routine-control-runs.component')
       .then((m) => m.RoutineControlRunsComponent),
