8978ca5b407e5b23647e65e74689e705f97ab745
VladRoJa <vladimirodriguez2329@gmail.com>
2026-09-12 13:53:52 -0700

    chore(planner): remove unused rxjs import

--- frontend/src/app/maintenance-planner/maintenance-planner.service.ts
+++ frontend/src/app/maintenance-planner/maintenance-planner.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, of, switchMap } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { SessionService } from '../core/auth/session.service';
 import { TicketService } from '../services/ticket.service';
