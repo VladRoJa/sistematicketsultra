0acfa6a96e2ba27149a0e66ab646c59dfdee9fd0
VladRoJa <vladimirodriguez2329@gmail.com>
2026-09-17 14:26:20 -0700

    feat(marketing): allow regional managers to view sales funnel

--- frontend/src/app/layout/layout.component.ts
+++ frontend/src/app/layout/layout.component.ts
@@ -1323,6 +1323,7 @@ private puedeVerMarketingSalesFunnelPorRol(): boolean {
     username === 'ADMICORP'
     || rol === 'LECTOR_GLOBAL'
     || rol === 'GERENTE'
+    || rol === 'GERENTE_REGIONAL'
   );
 }
 
